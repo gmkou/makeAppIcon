3070545216d840d7e8a18572cc6626ab7b4c526a
Kosuke Nagano <gm.charlie@gmail.com>
2014-09-09 19:52:38 +0900

    Add icon filename as array

--- iconFileName.rb
+++ iconFileName.rb
@@ -1,2 +1,20 @@
 # file name and size
-$hello="Hello World"
+$hello = "hello world"
+$icon_array = [
+              { fname: "Icon-40.png", size: 40 }, 
+              { fname: "Icon-40@2x.png", size: 80} 
+#  "Icon-60.png" => 60, 
+#  "Icon-60@2x.png" => 120, 
+#  "Icon-72.png" => 72, 
+#  "Icon-72@2x.png" => 144, 
+#  "Icon-76.png" => 76, 
+#  "Icon-76@2x.png" => 152, 
+#  "Icon-Small-50.png" => 50, 
+#  "Icon-Small-50@2x.png" => 100,
+#  "Icon-Small.png" => 29, 
+#  "Icon-Small@2x.png" => 58, 
+#  "Icon.png" => 57, 
+#  "Icon@2x.png" => 114, 
+#  "iTunesArtwork.png" => 512, 
+#  "iTunesArtwork@2x.png" => 1024
+]
