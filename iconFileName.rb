e529157533237738cf3a0ad17b6875f348a436a4
Kosuke Nagano <gm.charlie@gmail.com>
2014-09-09 19:56:13 +0900

    update icon file names

--- iconFileName.rb
+++ iconFileName.rb
@@ -1,20 +1,20 @@
 # file name and size
 $hello = "hello world"
 $icon_array = [
-              { fname: "Icon-40.png", size: 40 }, 
-              { fname: "Icon-40@2x.png", size: 80} 
-#  "Icon-60.png" => 60, 
-#  "Icon-60@2x.png" => 120, 
-#  "Icon-72.png" => 72, 
-#  "Icon-72@2x.png" => 144, 
-#  "Icon-76.png" => 76, 
-#  "Icon-76@2x.png" => 152, 
-#  "Icon-Small-50.png" => 50, 
-#  "Icon-Small-50@2x.png" => 100,
-#  "Icon-Small.png" => 29, 
-#  "Icon-Small@2x.png" => 58, 
-#  "Icon.png" => 57, 
-#  "Icon@2x.png" => 114, 
-#  "iTunesArtwork.png" => 512, 
-#  "iTunesArtwork@2x.png" => 1024
+               { fname: "Icon-40.png", size: 40 }, 
+               { fname: "Icon-40@2x.png", size: 80}, 
+               { fname: "Icon-60.png" , size: 60}, 
+               { fname: "Icon-60@2x.png" , size: 120}, 
+               { fname: "Icon-72.png" , size: 72}, 
+               { fname: "Icon-72@2x.png" , size: 144}, 
+               { fname: "Icon-76.png" , size: 76}, 
+               { fname: "Icon-76@2x.png" , size: 152}, 
+               { fname: "Icon-Small-50.png" , size: 50}, 
+               { fname: "Icon-Small-50@2x.png" , size: 100},
+               { fname: "Icon-Small.png" , size: 29}, 
+               { fname: "Icon-Small@2x.png" , size: 58}, 
+               { fname: "Icon.png" , size: 57}, 
+               { fname: "Icon@2x.png" , size: 114}, 
+               { fname: "iTunesArtwork.png" , size: 512}, 
+               { fname: "iTunesArtwork@2x.png" , size: 1024}
 ]
